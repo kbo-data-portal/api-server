02970f91249db93d60d6c9d0ee80a3d325a1a7f4
Rooam Lee <leewr9@naver.com>
2025-04-19 17:35:05 +0900

    portal:: add match data

--- fetcher.py
+++ fetcher.py
@@ -1,24 +1,73 @@
 from datetime import datetime, timedelta
-from sqlalchemy import create_engine, MetaData, Table, select, and_
+from sqlalchemy import create_engine, MetaData, Table, select, and_, func, over
+from sqlalchemy.sql import desc
 
 engine = create_engine("postgresql+psycopg2://postgres:postgres@localhost:5432/postgres")
-
 metadata = MetaData(schema="public")
 
-def fetch_data():
+def fetch_game_list():
     today = datetime.today()
-    before = today - timedelta(days=368)
-    after = today - timedelta(days=358)
-    schedule_tbl = Table("schedule", metadata, autoload_with=engine)
+    before = today - timedelta(days=3)
+    after = today + timedelta(days=7)
+    tbl = Table("game_schedule", metadata, autoload_with=engine)
     with engine.connect() as conn:
         query = (
-            select(schedule_tbl)
+            select(
+                tbl.c.G_ID,
+                tbl.c.G_DT_TXT,
+                tbl.c.G_TM,
+                tbl.c.S_NM,
+                tbl.c.AWAY_ID,
+                tbl.c.AWAY_NM,
+                tbl.c.HOME_ID,
+                tbl.c.HOME_NM,
+                func.coalesce(tbl.c.T_PIT_P_NM, "미정").label("T_PIT_P_NM"),
+                func.coalesce(tbl.c.B_PIT_P_NM, "미정").label("B_PIT_P_NM"),
+                tbl.c.T_PIT_P_NM,
+                tbl.c.B_PIT_P_NM,
+                tbl.c.SEASON_ID
+            )
             .where(and_(
-                schedule_tbl.c.SEASON_ID == 2024,
-                schedule_tbl.c.G_DT > before.strftime("%Y%m%d"),
-                schedule_tbl.c.G_DT < after.strftime("%Y%m%d"),
+                tbl.c.G_DT > before.strftime("%Y%m%d"),
+                tbl.c.G_DT < after.strftime("%Y%m%d"),
             ))
-           .order_by(schedule_tbl.c.G_DT)
+           .order_by(tbl.c.G_DT)
         )   
-        result = conn.execute(query).fetchall()
-        return result
+        return conn.execute(query).fetchall()
+
+
+def fetch_match_info(game_id):
+    tbl = Table("game_schedule", metadata, autoload_with=engine)
+    with engine.connect() as conn:
+        query = (
+            select(
+                tbl.c.SEASON_ID,
+                tbl.c.AWAY_ID,
+                tbl.c.AWAY_NM,
+                tbl.c.HOME_ID,
+                tbl.c.HOME_NM,
+                tbl.c.G_DT_TXT,
+                tbl.c.TV_IF,
+                tbl.c.S_NM
+            )
+            .where(tbl.c.G_ID == game_id)
+        )   
+        return conn.execute(query).fetchone()
+
+
+def fetch_rank_info(season):
+    tbl = Table("team_summary", metadata, autoload_with=engine)
+    with engine.connect() as conn:
+        query = (
+            select(
+                tbl.c.TEAM_NM,
+                tbl.c.W_CN,
+                tbl.c.L_CN,
+                tbl.c.D_CN,
+                func.rank().over(order_by=desc(tbl.c.W_RATE)).label("RANK")
+            )
+            .where(and_(
+                tbl.c.SEASON_ID == season
+            ))
+        )   
+        return conn.execute(query).fetchall()
--- static/js/script.js
+++ static/js/script.js
@@ -1,51 +1,113 @@
-craetePie('graph-all-match-left', '<span>172<br/>GAME</span>',
-  [
-    { name: 'KT 위즈 승', y: 65, color: homeColor },
-    { name: '무', y: 49, color: '#666666' },
-    { name: '삼성 라이온즈 승', y: 63, color: awayColor },
-  ]
-);
+$(document).on("click", ".game-cont", function () {
+  const gameId = $(this).data("game-id");
 
-createColumn('graph-all-match-right', '', ['득점'],
-  [
-    { name: '삼성 라이온즈', data: [45], color: awayColor },
-    { name: 'KT 위즈', data: [31], color: homeColor }
-  ]
-);
+  $.ajax({
+    url: "/get_match",
+    method: "POST",
+    contentType: "application/json",
+    data: JSON.stringify({ game_id: gameId }),
+    success: function (response) {
+      const matchBox = $(".match-box");
+      matchBox.empty();
+      matchBox.append(`
+          <span class="match-box-title">장소</span>
+          <span class="match-box-txt">${response.S_NM}</span>
+          <span class="match-box-title">경기일자</span>
+          <span class="match-box-txt">${response.G_DT_TXT}</span>
+          <span class="match-box-title">방송중계</span>
+          <span class="match-box-txt">${response.TV_IF}</span>
+      `);
 
-craetePie('graph-match-left', '<span>10<br/>GAME</span>',
-  [
-    { name: 'KT 위즈 승', y: 4, color: homeColor },
-    { name: '무', y: 3, color: '#666666' },
-    { name: '삼성 라이온즈 승', y: 3, color: awayColor },
-  ]
-);
+      const teamHome = $(".team.home.info");
+      teamHome.empty();
+      teamHome.append(`
+      <div class="name" style="color: ${response.HOME_COLOR};">${response.HOME_NM}</div>
+      <div class="score">${response.HOME_SCORE}</div>
+      <div class="emb">
+        <img src="//6ptotvmi5753.edge.naverncp.com/KBO_IMAGE/KBOHome/resources/images/emblem/regular/2022/${response.HOME_ID}.png" alt="" />
+      </div>
+      `);
 
-createColumn('graph-match-right', '', ['득점'],
-  [
-    { name: '삼성 라이온즈', data: [45], color: awayColor },
-    { name: 'KT 위즈', data: [31], color: homeColor }
-  ]
-);
+      const teamAway = $(".team.away.info");
+      teamAway.empty();
+      teamAway.append(`
+        <div class="emb">
+          <img src="//6ptotvmi5753.edge.naverncp.com/KBO_IMAGE/KBOHome/resources/images/emblem/regular/2022/${response.AWAY_ID}.png" alt="" />
+        </div>
+        <div class="score">${response.AWAY_SCORE}</div>
+        <div class="name" style="color: ${response.AWAY_COLOR};">${response.AWAY_NM}</div>
+      `);
 
-craeteButterflyBar('graph-stat-leftL', 'graph-stat-leftR', '',
-  ['홈런', '안타', '타점', '득점권 타율'],
-  [{
-    data: [27, 148, 91, 6.298],
-    color: awayColor
-  }], [{
-    data: [14, 58, 121, 11],
-    color: homeColor
-  }]
-);
+      
+      craetePie('graph-all-match-left', '<span>172<br/>GAME</span>',
+        [
+          { name: `${response.HOME_NM} 승`, y: 65, color: response.HOME_COLOR },
+          { name: '무', y: 49, color: '#666666' },
+          { name: `${response.AWAY_NM} 승`, y: 63, color:response.AWAY_COLOR },
+        ]
+      );
 
-craeteButterflyBar('graph-stat-rightL', 'graph-stat-rightR', '',
-  ['홈런', '안타', '타점', '득점권 타율'],
-  [{
-    data: [27, 148, 91, 6.298],
-    color: awayColor
-  }], [{
-    data: [14, 58, 121, 11],
-    color: homeColor
-  }]
-);
+      createColumn('graph-all-match-right', '', ['득점'],
+        [
+          { name: response.AWAY_NM, data: [45], color: response.AWAY_COLOR },
+          { name: response.HOME_NM, data: [31], color: response.HOME_COLOR }
+        ]
+      );
+
+      craetePie('graph-match-left', '<span>10<br/>GAME</span>',
+        [
+          { name: `${response.HOME_NM} 승`, y: 4, color: response.HOME_COLOR },
+          { name: '무', y: 3, color: '#666666' },
+          { name: `${response.AWAY_NM} 승`, y: 3, color: response.AWAY_COLOR },
+        ]
+      );
+
+      createColumn('graph-match-right', '', ['득점'],
+        [
+          { name: response.AWAY_NM, data: [45], color: response.AWAY_COLOR },
+          { name: response.HOME_NM, data: [31], color: response.HOME_COLOR }
+        ]
+      );
+
+      craeteButterflyBar('graph-stat-leftL', 'graph-stat-leftR', '',
+        ['안타', '홈런', '타점', '득점'],
+        [{
+          data: [27, 148, 91, 6.298],
+          color: response.AWAY_COLOR
+        }], [{
+          data: [14, 58, 121, 11],
+          color: response.HOME_COLOR
+        }]
+      );
+
+      craeteButterflyBar('graph-stat-rightL', 'graph-stat-rightR', '',
+        ['볼넷', '도루', '삼진', '장타'],
+        [{
+          data: [27, 148, 91, 6.298],
+          color: response.AWAY_COLOR
+        }], [{
+          data: [14, 58, 121, 11],
+          color: response.HOME_COLOR
+        }]
+      );
+
+      const matchList = $(".match-list");
+      matchList.empty();
+      response.matches.forEach(match => {
+        const li = `
+            <li>
+              <span class="match-name away-name" style="color: ${match.away_color};">${match.away}</span>
+              <span class="match-score away-score" style="background: ${match.away_color};">${match.away_score}</span>
+              <span class="match-date">${match.date}</span>
+              <span class="match-score home-score" style="background: ${match.home_color};">${match.home_score}</span>
+              <span class="match-name home-name" style="color: ${match.home_color};">${match.home}</span>
+            </li>
+          `;
+        matchList.append(li);
+      });
+    },
+    error: function (e) {
+      console.error(e);
+    }
+  });
+});
